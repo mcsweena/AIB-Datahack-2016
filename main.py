912c4ac8e9aaa5dc1f14029cc1e4751b930342fb
mcsweena <andy.mcsweeney91@gmail.com>
2017-03-28 18:55:41 +0100

    Adding README.md, some data and new main.py file

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn import cross_validation
+
+
+def data_import():
+    pass
+
+
+def cross_val():
+    pass
+
+
+def Logreg():
+    pass
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
